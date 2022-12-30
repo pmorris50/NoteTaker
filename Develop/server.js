b14f924ce21fa0f34c8d709e5fb18d62a0462927
pmorris50 <patrickmorris532@gmail.com>
2022-12-30 10:07:06 -0700

    quicksave

--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,11 @@ const fs = require('fs');
 // const { clog } =require() //What is clog? why is it used?
 const app = express();
 const PORT = process.env.port || 3001;
-const { v4: uuidv4} = require('uuid');
+//const { v4: uuidv4} = require('uuid');
+
+const uuid = require('uuid');
+console.log(`Here is a test v1 uuid: ${uuid.v1()}`); //use this one
+console.log(`Here is a test v4 uuid: ${uuid.v4()}`);
 app.listen(PORT, ()=> {
     console.log(`now listening on port ${PORT}`)
 });
@@ -28,13 +32,36 @@ app.get('*', (req, res) => {
 
 //Get Route to return all saved notes as JSON
 app.get('api/notes', (req, res) => {
+    fs.readFile('db.json', 'utf8', (err, data) => {
+        if(err) throw err;
+        const notes = JSON.parse(data);
+        res.json(notes);
+    })
    // res.json(//all saved notes)
 })
 
 app.post('api/notes', (res, req) => {
+    const { noteTitle, noteBody} = req.body
+    console.log(req.body);
+    if(req.body){
+        const newNote = {
+            noteTitle,
+            noteBody,
+            noteID: uuidv1(), //giving a unique id to each note
+        };
+        readAndAppend(newNote, './db.json'); //where do I send the note?
+        res.json('Note added succussfully');
+
+    } else{
+        res.error('error in submitting the note')
+    }
+
     //add a new note to the db.json file and return new note to client
 })
 
 app.delete('/api/notes/:id', (res, req) =>{
+    const notesID = req.params.id
+    readFromFile('./db.json')
+    .then((data) => JSON.parse(data))
     //delete notes, give each note a specific ID number 
 })
